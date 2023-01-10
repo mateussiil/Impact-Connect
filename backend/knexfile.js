9864a2bca3628182283b49ac359c40de362046c5
Mateus Silva <mateus_silva97@hotmail.com>
2023-01-10 10:04:28 -0300

    feat: added crossenv and .env

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -32,9 +32,9 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: process.env.DB_DATABASE,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
     },
     pool: {
       min: 2,
